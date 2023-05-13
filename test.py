f71950ddcdb7782d03358240cb5ff714e31350db
Smaranda02 <62556419+Smaranda02@users.noreply.github.com>
2023-05-13 12:36:01 +0300

    weather api added test.py

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
+#to do 
+#hosting, music, quiz, quote time
+
+
 import os
 import discord
 from dotenv import load_dotenv
-from discord.ext import commands
+from nextcord.ext import commands,tasks
 from dataclasses import dataclass
 import youtube_dl
 import aiohttp  #for gpt
@@ -9,28 +13,130 @@ import asyncio  #for music
 import time
 import schedule 
 import pyjokes
+import random
+import datetime
+import requests
+import json
+from gtts import gTTS
+import nextcord
+import pyowm #wrapper for weather api
 
 
-intents = discord.Intents.all()
+
+
+intents = nextcord.Intents.all()
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 #CHANNEL=os.getenv('CHANNEL_ID')
 CHANNEL=1102597234832453816
-API_KEY='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'
+API_KEY=os.getenv('API_KEY')
+api_key_weather=os.getenv('WEATHER_API_KEY')
+
 
 voice_clients={}
 
 yt_dl_opts={'format':'bestaudio/best'}
 ytdl=youtube_dl.YoutubeDL(yt_dl_opts)
 
+
 ffmpeg_options={'options':"-vn"}
 
 
 #setting variables 
-client = discord.Client(intents=intents)
+client = nextcord.Client(intents=intents)
 bot=commands.Bot(command_prefix="!",intents=intents)
 
+channel=bot.get_channel(CHANNEL)
+
+utc=datetime.timezone.utc
+time=datetime.time(hour=22, minute = 12, tzinfo=utc)
+
+
+
+#owm = pyowm.OWM('4b096846dea87214c945bf7a0158fe48')
+
+
+
+
+@bot.command()
+async def weather(context, city_name): 
+    base_url = "http://api.openweathermap.org/data/2.5/weather?"
+    city=city_name
+    complete_url = base_url + "appid=" + api_key_weather + "&q=" + city
+    response=requests.get(complete_url)
+    res=response.json()
+    info=res["main"]
+
+    if response.status_code!=200:
+        await context.send("Invalid city name. Try again!")
+
+    else:
+        await context.send(f'''Temperature in Celsius in {city_name} is : { int(info['temp']-273) }
+                        How the temperature actually feels like : { int(info['feels_like']-273)}
+                        Short description of the weather : { res['weather'][0]['description']}
+                        Pressure : { info['pressure']}\nHumidity : { info['humidity']}
+                        ''')
+
+
+
+
+
+
+@bot.command()
+async def tts(context, *args):
+    text=" ".join(args)
+    user=context.message.author
+    if user.voice!= None:
+        try:
+            vc= await user.voice.channel.connect()
+        except: 
+            vc=context.voice_client
+
+        sound=gTTS(text=text,lang="en", slow=False)
+        sound.save("tts-audio.mp3")
+
+        if vc.is_playing():
+            vc.stop()
+
+        source=await nextcord.FFmpegOpusAudio.from_probe("tts-audio.mp3", method="fallback")
+        vc.play(source)
+    
+    else:
+        await context.send("You need to be in a vc to run the command")
+
+
+
+
+
+@bot.event
+async def quote_of_the_day():
+        @tasks.loop(seconds=2)
+        async def quote():
+            response = requests.get ("https://zenquotes.io/api/random")
+            json_data = json.loads(response.text)
+            quote = json_data[0]['q'] + " -" + json_data[0]['a']
+            await channel.send(quote)
+
+
+@bot.event
+async def on_member_join(member):
+    channel=bot.get_channel(CHANNEL)
+    await channel.send(f"{member.mention} has joined the server")
+    
+
+@bot.event
+async def on_member_remove(member):
+    channel=bot.get_channel(CHANNEL)
+    await channel.send(f"{member.mention} has left the server")
+
+
+@bot.event
+async def on_member_remove(member):
+    channel=bot.get_channel(CHANNEL)
+    await channel.send(f"{member.mention} has left the server")
+        
+        
 
 @bot.command()
 async def joke(context):
@@ -92,7 +198,7 @@ async def gpt(context:commands.Context, *, prompt: str):
 
         async with session.post("https://api.openai.com/v1/completions", json=payload , headers=headers) as resp:
             response = await resp.json()
-            embed =discord.Embed(title="Chat GPT'S Response ", description=response["choices"][0]["text"])
+            embed =nextcord.Embed(title="Chat GPT'S Response ", description=response["choices"][0]["text"])
             await context.reply(embed=embed)
 
 
